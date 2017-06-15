4ded3b51ca3380c53b3c09e861752a1be5c86f92
yjyao <yuanjun.yao@duke.edu>
2017-06-14 20:09:58 -0400

    spark

---
+++ spark.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#run sliceName spark WorkerNum
+./target/appassembler/bin/SafeSdxExample ~/.ssl/geni-yuanjuny.pem ~/.ssl/geni-yuanjuny.pem "https://geni.renci.org:11443/orca/xmlrpc" $1 $2 $3 $4
+
