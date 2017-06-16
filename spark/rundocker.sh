7a6a856d9d7419986fb0491a8bd6ac6792be9f88
yjyao <yuanjun.yao@duke.edu>
2017-06-15 20:43:36 -0400

    download from registry

--- spark/rundocker.sh
+++ spark/rundocker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker run -i -t  -d -p 4040:4040 -p 6066:6066 -p 7077:7077 -p 8080:8080 -p 8081:8081   -h sparkserver --name sparkserver sparkimg
+sudo docker run -i -t  -d -p 4040:4040 -p 6066:6066 -p 7077:7077 -p 8080:8080 -p 8081:8081   -h sparkserver --name sparkserver yaoyj11/sparkimg
